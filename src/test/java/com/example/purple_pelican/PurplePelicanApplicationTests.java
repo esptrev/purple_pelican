dcc22b3d685767b857688bf86da0dcc9ae1c5041
Trevor Esparza <trevoresparza30@gmail.com>
2022-07-26 07:19:57 -0500

    spring/react setup

--- src/test/java/com/example/purple_pelican/PurplePelicanApplicationTests.java
+++ src/test/java/com/example/purple_pelican/PurplePelicanApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PurplePelicanApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
