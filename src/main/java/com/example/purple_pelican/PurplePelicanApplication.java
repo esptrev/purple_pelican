f0fe7d38456152d4f4db5a0519944f0229fdfdc9
Trevor Esparza <trevoresparza30@gmail.com>
2022-07-26 07:19:38 -0500

    first commit

---
+++ src/main/java/com/example/purple_pelican/PurplePelicanApplication.java
@@ -0,0 +1,13 @@
+package com.example.purple_pelican;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurplePelicanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurplePelicanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/purple_pelican/PurplePelicanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.purple_pelican;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurplePelicanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
